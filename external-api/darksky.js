4e627a84a77cc455aea7cb66a43c671b0ebd6060
Chief Wizard <40777040+chief-wizard@users.noreply.github.com>
2019-07-23 17:16:31 +0200

    No need for AWS credentials with DarkSky

--- external-api/darksky.js
+++ external-api/darksky.js
@@ -1,19 +1,13 @@
 const fetch = require("node-fetch");
 
-var AWS = require('aws-sdk'),
-    region = process.env.AWS_REGION_ENV,
-    darkskyApiKey = process.env.DARKSKY_APIKEY,
-    accessKeyId = process.env.ACCESS_KEY_ID,
-    secretAccessKey = process.env.SECRET_ACCESS_KEY;
+var apiUrl = process.env.DARKSKY_URL;
+var apiKey = process.env.DARKSKY_APIKEY;
 
 exports.darksky = async (event, context, callback) => {
     return callExternalApi(event.pathParameters.latitude, event.pathParameters.longitude);
 }
 
 var callExternalApi = async function (latitude, longitude) {
-    var apiUrl = process.env.DARKSKY_URL;
-    var apiKey = darkskyApiKey;
-
     try {
         var response = await fetch(`${apiUrl}/${apiKey}/${latitude},${longitude}`);
         var json = await response.json();
